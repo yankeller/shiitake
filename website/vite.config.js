5c09b734bc92cce4874e7dbd3fcfb6adaf4baf7c
Keller Yan <205859407+yankeller@users.noreply.github.com>
2026-01-01 16:51:21 -0500

    Add basic landing page (#2)
    * feat: add minimalist landing page
    
    * ci: add website check

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { sveltePhosphorOptimize } from "phosphor-svelte/vite";
 
 export default defineConfig({
     plugins: [
         sveltekit(),
-        tailwindcss()
+        tailwindcss(),
+        sveltePhosphorOptimize()
     ],
     build: {
         target: "ES2022"
     },
     server: {
-        port: 5173,
-        fs: {
-            allow: [".."]
-        }
+        port: 5174
     }
 });
